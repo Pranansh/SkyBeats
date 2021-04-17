15f3394c24a398257b076c3f2a1b1e078eef89ef
aadhya002 <72016164+aadhya002@users.noreply.github.com>
2021-04-17 20:10:53 +0530

    Update Manager_Queries.sql

--- Manager_Queries.sql
+++ Manager_Queries.sql
@@ -1,55 +1,49 @@
--- View list of Producers being managed by them
-SELECT   manager_managerID,producer.producerID,producer.views
-FROM        manager 
-INNER JOIN  producer  
-ON      producer.manager_managerID= manager.managerID;
-
--- average of total number of views of all the producer under a manager
-SELECT   manager_managerID, ROUND(AVG(CAST(producer.views AS FLOAT)), 2)
-FROM        manager 
-INNER JOIN  producer  
-ON     producer.manager_managerID= manager.managerID
-GROUP BY manager.managerID;
-
-
 -- number of producer under a manager
-SELECT   manager_managerID, COUNT(producer.producerID),manager.numberofproducer
+SELECT   manager_managerID, COUNT(producer.producerID)
 FROM        manager 
 INNER JOIN  producer  
 ON     producer.manager_managerID= manager.managerID
+WHERE manager.managerID='M1003'
 GROUP BY manager.managerID;
 
--- view rating of all songs under the manager
-SELECT   manager_managerID, ROUND(AVG(CAST(songs.rating AS FLOAT)), 2)
+-- View list of Producers being managed by a manager
+SELECT   manager_managerID,producer.producerID
 FROM        manager 
 INNER JOIN  producer  
-ON     producer.manager_managerID= manager.managerID
-INNER JOIN  songs 
-ON     producer.producerID= producer_producerID
-GROUP BY manager.managerID;
+ON      producer.manager_managerID= manager.managerID
+WHERE manager.managerID='M1003';
 
 -- Earnings
-SELECT managerID , earnings FROM manager; 
+SELECT managerID , earnings FROM manager WHERE manager.managerID='M1003'; 
 
 -- the producer with highest rating under a manager
-
-SELECT   manager_managerID , max(songs.rating) , producer_producerID ,songs.songID
-FROM     manager 
-INNER JOIN  producer  
-ON     producer.manager_managerID= manager.managerID
-INNER JOIN  songs 
-ON     producer.producerID= producer_producerID 
-GROUP BY manager.managerID
-ORDER BY manager.managerID;
-
--- total views of a all the producer under a given manager
-SELECT   producer.producerID, SUM(songs.views),producer.manager_managerID
+SELECT   producer.manager_managerID,producer.producerID,songs.songID,songs.rating
 FROM        producer
 INNER JOIN  songs
 ON      producer.producerID=  songs.producer_producerID 
 WHERE producer.manager_managerID='M1003'
 GROUP BY producer.producerID
-ORDER BY producer.producerID;
+ORDER BY songs.rating desc
+limit 1;
+
+-- the producer with highest views under a manager
+SELECT   producer.manager_managerID,producer.producerID,sum(songs.views)
+FROM        producer
+INNER JOIN  songs
+ON      producer.producerID=  songs.producer_producerID 
+WHERE producer.manager_managerID='M1003'
+GROUP BY producer.producerID
+ORDER BY songs.views desc
+limit 1;
+
+-- the performance of each producer under a manager
+SELECT   producer.producerID, SUM(songs.views),producer.manager_managerID,songs.songID ,songs.rating
+FROM        producer
+INNER JOIN  songs
+ON      producer.producerID=  songs.producer_producerID 
+WHERE producer.manager_managerID='M1003'
+GROUP BY producer.producerID
+ORDER BY songs.views;
  
 -- total views of a single manager
 SELECT   manager_managerID , sum(songs.views)
@@ -58,6 +52,7 @@ INNER JOIN  producer
 ON     producer.manager_managerID= manager.managerID
 INNER JOIN  songs 
 ON     producer.producerID= producer_producerID 
+WHERE manager.managerID='M1003'
 GROUP BY manager.managerID
-ORDER BY manager.managerID;
+ORDER BY songs.views;
 
