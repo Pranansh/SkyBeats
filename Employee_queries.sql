a82543195069730d33217bec122ad5c52af12462
Riya Sogani <riya19442@iiitd.ac.in>
2021-04-18 16:40:23 +0530

    Update Employee_queries.sql

--- Employee_queries.sql
+++ Employee_queries.sql
@@ -2,7 +2,7 @@
 --View individual earning:
 SELECT salary
 	FROM employee
-	WHERE employeeID="E1000"
+	WHERE employeeID=eID
 
 --View all pending customer complaints:
 	SELECT complainID, reason, customer_customerID
@@ -15,13 +15,13 @@ SELECT salary
 --View top 5 most played songs:
 SELECT songID
 	FROM (SELECT songID
-FROM songs
-ORDER BY views desc) 
-	WHERE rownum <=5
+		FROM songs
+		ORDER BY views desc) as sub
+	LIMIT 5
 	
 --View all resolved customer complaints:
 --I'm a bit confused about this because we didn't give names to the attrubutes of process
 SELECT complainID, reason, customer_customerID
 	FROM process
 	INNER JOIN complain
-	WHERE employeeID="E1000"
+	WHERE employee_employeeID=eID
